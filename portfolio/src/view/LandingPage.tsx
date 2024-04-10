79a5b2a7b9afbfce8f062eba810562e4bcbbafcf
itahTamar <125893355+itahTamar@users.noreply.github.com>
2024-04-10 11:27:28 +0300

    start

---
+++ portfolio/src/view/LandingPage.tsx
@@ -0,0 +1,14 @@
+import Navbar from '../component/Navbar'
+
+const LandingPage = () => {
+  return (
+    <>
+    <Navbar/>
+    <div className="container-about">About Me</div>
+    <div className="container-projects">My Projects</div>
+    <div className="container-contact">Contact Me</div>
+    </>
+  )
+}
+
+export default LandingPage
